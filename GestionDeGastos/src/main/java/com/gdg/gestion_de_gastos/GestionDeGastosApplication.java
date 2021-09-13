4623240abb55956654d9956969d04b774e0c6286
Usuario <Usuario@DESKTOP-22TK799>
2021-09-13 11:12:40 +0200

    Inicio proyecto

---
+++ GestionDeGastos/src/main/java/com/gdg/gestion_de_gastos/GestionDeGastosApplication.java
@@ -0,0 +1,13 @@
+package com.gdg.gestion_de_gastos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDeGastosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDeGastosApplication.class, args);
+	}
+
+}
---
+++ GestionDeGastos/src/test/java/com/gdg/gestion_de_gastos/GestionDeGastosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdg.gestion_de_gastos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeGastosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
